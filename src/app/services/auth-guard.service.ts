1939aa0d4685537b30f752a95f017643c6697858
Youssouf.T <youssouftoure2007@gmail.com>
2021-04-15 16:38:00 +0200

    Mise à jour de HeaderComponent : typeScript & template Mise à jour de AuthGuardService & creation canActivate Mise de AppModule : Routing

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  	constructor(private router: Router) { }
+	
+	canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+		return new Promise((resolve, reject) => {
+			firebase.auth().onAuthStateChanged(
+				(user) => {
+					if (user) {
+						resolve(true);
+					} else {
+						this.router.navigate(['/auth', 'signin']);
+						resolve(false);
+					}
+				}
+			);
+		});
+	}
 }
