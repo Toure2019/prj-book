748dc07ede601475efe3c0b8214e4585e4fa221a
Youssouf.T <youssouftoure2007@gmail.com>
2021-04-15 13:49:14 +0200

    Installation & configuratuion de Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app'; 	// Doit être toujours importé en premier
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  	selector: 'app-root',
+  	templateUrl: './app.component.html',
+  	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prj-book';
+  	title = 'prj-book';
+	constructor() {
+		const config = {
+			apiKey: "AIzaSyBXn97TSIe4ilQ7NejiXS6eWRGEb_gcJ1c",
+			authDomain: "prj-book-2021.firebaseapp.com",
+			databaseURL: "https://prj-book-2021.firebaseio.com",
+			projectId: "prj-book-2021",
+			storageBucket: "prj-book-2021.appspot.com",
+			messagingSenderId: "856793800231",
+			appId: "1:856793800231:web:1b4d6262a58a55e91b3802",
+			measurementId: "G-G9WSQC43K1"
+		};
+		firebase.initializeApp(config);
+	}
 }
