8660328bea84156d139efe5dffe5564a5bef4029
Youssouf.T <youssouftoure2007@gmail.com>
2021-04-15 17:31:25 +0200

    Mise à jour de BookListComponent

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Book } from '../models/book.model';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+	books: Book[];
+	bookSubscription: Subscription;
 
-  ngOnInit(): void {
-  }
+  	constructor(private bookService: BooksService, private router: Router) { }
 
+  	ngOnInit(): void {
+		this.bookSubscription = this.bookService.bookSubject.subscribe(
+			(books: Book[]) => {
+				this.books = books;
+			}
+		);
+		this.bookService.emitBooks();
+  	}
+	
+	onNewBook() {
+		this.router.navigate(['/books', 'new']);
+	}
+
+	onDeleBook(book: Book) {
+		this.bookService.removeBook(book);
+	}
+
+	onViewBook(id: number) {
+		this.router.navigate(['/books', 'view', id]);
+	}
+
+	ngOnDestroy() {
+		this.bookSubscription.unsubscribe();
+	}
 }
