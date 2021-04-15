49108d17963c074b011a0c9639d2508d54e15b62
Youssouf.T <youssouftoure2007@gmail.com>
2021-04-15 16:14:33 +0200

    Creation des méthodes de AuthService & Mise à jour de SignUpComponent & SignInComponent

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  	constructor() { }
+
+	createNewUser(email: string, password: string) {
+		return new Promise((resolve, reject) => {
+			firebase.auth().createUserWithEmailAndPassword(email, password).then(
+				(value) => {
+					resolve(value);
+				},
+				(error) => {
+					reject(error);
+				}
+			);
+		});
+	}
+
+	signInUser(email: string, password: string) {
+		return new Promise((resolve, reject) => {
+			firebase.auth().signInWithEmailAndPassword(email, password).then(
+				(value) => {
+					resolve(value);
+				},
+				(error) => {
+					reject(error);
+				}
+			);
+		});
+	}
+
+	signOutUser() {
+		firebase.auth().signOut();
+	}
 }
