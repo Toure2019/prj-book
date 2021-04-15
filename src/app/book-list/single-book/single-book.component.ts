826dec8edc31828844dbc3b6628ae907a60ce016
Youssouf.T <youssouftoure2007@gmail.com>
2021-04-15 17:38:12 +0200

    Mise à jour de SingleBookComponent

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from 'src/app/models/book.model';
+import { BooksService } from 'src/app/services/books.service';
 
 @Component({
   selector: 'app-single-book',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBookComponent implements OnInit {
 
-  constructor() { }
+	book: Book;
 
-  ngOnInit(): void {
-  }
+  	constructor(private route: ActivatedRoute, private bookService: BooksService, private router: Router) { }
 
+  	ngOnInit(): void {
+		this.book = new Book('', '');
+		const id = this.route.snapshot.params['id'];
+		this.bookService.getSignleBook(+id).then(
+			(book: Book) => {
+				this.book = book;
+			}
+		);
+  	}
+
+	onBack() {
+		this.router.navigate(['/books']);
+	}
 }
